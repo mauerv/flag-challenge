d0b99ea3f7a4a42b150ae89f5ba31f7ca5065270
mauerv <maurovelazquez1@gmail.com>
2019-04-23 14:32:18 -0300

    Improve styling and naming

--- src/App.js
+++ src/App.js
@@ -31,69 +31,73 @@ export default class App extends Component {
 
     return (
       <div className='app'>
-        <h1>Flag Picker</h1>
-        <p>This app will help you learn flags around the world in 3 steps.</p>
-        <div className='step'>
-          <div className='header'>
-            <h1 className='title'>Step 1</h1>
-            <p className='subtitle'>Select a continent</p>
+        <div className='header'>
+          <h1 className='title'>Flag Picker</h1>
+          <p className='subtitle'>This app will help you learn flags around the world in 3 steps.</p>
+        </div>
+        <div className='step-container'>
+          <div className='step'>
+            <h1 className='step-title'>Step 1</h1>
+            <p className='step-description'>Select a continent</p>
+            <input 
+              type='text'
+              value={text}
+              onChange={this.onChange}
+              onFocus={this.onContinentFocus}
+              className='step-input'
+            />
+            {continentFocus && (
+              <ul className='list'>
+                {data.map(item => {
+                  if (text.charAt(0).toUpperCase() === item.continent.substr(0, text.length)) {
+                    return <li className='list-item' key={item.continent} onClick={() => this.onContinentSelect(item.continent)}>{item.continent}</li>
+                  }
+                })}
+              </ul>
+            )}
+            {continent && (
+              <div>
+                <p>You selected</p>
+                <h1>{continent}</h1>
+              </div>
+            )}
           </div>
-          <input 
-            type='text'
-            value={text}
-            onChange={this.onChange}
-            onFocus={this.onContinentFocus}
-          />
-          {continentFocus && (
-            <ul>
-              {data.map(item => {
-                if (text.charAt(0).toUpperCase() === item.continent.substr(0, text.length)) {
-                  return <li key={item.continent} onClick={() => this.onContinentSelect(item.continent)}>{item.continent}</li>
-                }
-              })}
-            </ul>
-          )}
           {continent && (
-            <div>
-              <p>You selected</p>
-              <h1>{continent}</h1>
+            <div className='step'>
+              <h1>Step 2</h1>
+              <p>Now select a country.</p>
+              <input 
+                type='text' 
+                readOnly={true}
+                onFocus={this.onCountryFocus}
+                className='step-input'
+              />
+              {countryFocus && (
+                <ul className='list'>
+                  {data.map(item => item.continent === continent && (
+                    item.countries.map(country => (
+                      <div className='checkbox-item' key={country.name}>
+                        <input type='checkbox' onClick={e => this.onCountryToggle(e, country)}/>
+                        <label>{country.name}</label>
+                      </div>
+                    ))
+                  ))}
+                </ul>
+              )}
+            </div>
+          )}
+          {!!Object.keys(countries).length && (
+            <div className='step'>
+              <h1>Selected flags:</h1>
+              <div>
+                {Object.keys(countries).map(country => 
+                  !!countries[country] && <p key={country}>{country}</p>
+                )}
+              </div>
+              <button className='button' onClick={this.onClear}>Clear flags</button>
             </div>
           )}
         </div>
-        {continent && (
-          <div className='step'>
-            <h1>Step 2</h1>
-            <p>Now select a country.</p>
-            <input 
-              type='text' 
-              readOnly={true}
-              onFocus={this.onCountryFocus}
-            />
-            {countryFocus && (
-              <ul>
-                {data.map(item => item.continent === continent && (
-                  item.countries.map(country => (
-                    <div key={country.name}>
-                      <label>{country.name}</label>
-                      <input type='checkbox' onClick={e => this.onCountryToggle(e, country)}/>
-                    </div>
-                  ))
-                ))}
-              </ul>
-            )}
-          </div>
-        )}
-        {!!Object.keys(countries).length && (
-          <div className='step'>
-            <h1>Selected flags:</h1>
-            <div>
-              {Object.keys(countries).map(country => 
-                !!countries[country] && <p key={country}>{country}</p>
-              )}
-            </div>
-            <button onClick={this.onClear}>Clear flags</button>
-          </div>
-        )}
       </div>
     )
   }
